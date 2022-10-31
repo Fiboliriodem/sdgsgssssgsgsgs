0a2f2b99054303edb4d0a39438d0d6826ab8aa5d
Fiboliriodem <101954899+Fiboliriodem@users.noreply.github.com>
2022-10-31 10:22:39 -0300

    Update workshop.cpp

--- workshop.cpp
+++ workshop.cpp
@@ -1,7 +1,5 @@
-echo "# sdgsgssssgsgsgs" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Fiboliriodem/sdgsgssssgsgsgs.git
-git push -u origin main
+#include <stdio.h>
+
+int main(){
+  
+}
